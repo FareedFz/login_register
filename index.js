00d94a53387ce5c0378bcd7681279327d9aa8859
FareedFz <78958186+FareedFz@users.noreply.github.com>
2022-06-19 22:07:39 +0530

    add console.log

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ const router = require('./routes/appRouter');
 app.use('/user',router)
 app.listen(5206,()=>{
     console.log("server running on  thia  port 5206");
+    console.log("hello world");
 })
