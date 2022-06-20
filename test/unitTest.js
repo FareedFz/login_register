913b7789fb02d0106f449afbcc4cb633c937af08
FareedFz <78958186+FareedFz@users.noreply.github.com>
2022-06-21 00:19:21 +0530

    Update unitTest.js

--- test/unitTest.js
+++ test/unitTest.js
@@ -4,7 +4,7 @@ const getSumValues = require('../controller/userController')
 
 describe('getSumValues', function () {
     it("should return correct value",  () => {
-        let getData = getSumValues.sumValue(5)
+        let getData = getSumValues.sumValue(6)
         expect(getData).to.deep.equal(10);
     })
 })
